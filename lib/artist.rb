ae2fec21504b952fa96eab0927b0fed25ddf4279
Katie Hoffman <ktahoffman@gmail.com>
2014-08-22 14:56:40 -0400

    remove passing code

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,3 @@
-# Implement an artist class below.
-# The serialize method should replace spaces in 
-# the song title with underscores and write to 
-# the current working directory
 class Artist
-  attr_reader :name
-  def initialize(name)
-    @name = name
-  end
+  # code goes here
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,3 @@
-# Implement a song class below.
-# The serialize method should replace spaces in 
-# the song title with underscores and write to 
-# the current working directory
 class Song
-  attr_accessor :title, :artist
-
-  def serialize
-    new_title = self.title.gsub(" ", "_")
-    open("#{new_title}.txt", 'w') do |f|
-      f.puts "#{self.artist.name} - #{self.title}"
-    end 
-  end
+  # code goes here
 end
