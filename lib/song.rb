f8e03d79dc0d87f08712b25c4d7d327e6780ee7a
Katie Hoffman <ktahoffman@gmail.com>
2014-08-22 14:53:01 -0400

    passes all tests

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,9 @@ class Song
   attr_accessor :title, :artist
 
   def serialize
-
+    new_title = self.title.gsub(" ", "_")
+    open("#{new_title}.txt", 'w') do |f|
+      f.puts "#{self.artist.name} - #{self.title}"
+    end 
   end
 end
